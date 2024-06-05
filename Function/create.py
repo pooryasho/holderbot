0a97b2b6ef918e617fc4130cd79c5d311f4cf0cd
pooryasho <142620046+pooryasho@users.noreply.github.com>
2024-06-06 02:23:25 +0330

    create.py

--- Function/create.py
+++ Function/create.py
@@ -81,6 +81,6 @@ def DEF_USERNAME_STARTER(TEXT, MUCH_NUMBER):
     return RESULT_LIST
 
 def DEF_SEND_QR_TEXT(USER_SUB , USERNAME , DATA , DATE) :
-    TEXT = f"<pre>{USER_SUB}</pre>"
+    TEXT = f"👤 Username: {USERNAME}\n📅 Time (Days): {DATE}\n🪫 Data Usage (GB): {DATA}\n\n🖇 Subscription Link: {USER_SUB}"
     return TEXT
     
